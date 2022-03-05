04df40acbfea6ea13182683a131ac040031ddfe9
demon <demonsd98@outlook.com>
2022-03-05 12:14:29 +0000

    Reformat receiver and transmitter. Fix error in receiver state machine logic.

--- sources_1/new/MouseReceiver.v
+++ sources_1/new/MouseReceiver.v
@@ -1,186 +1,154 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: The University of Edinburgh
-// Engineer: Vincent Steil
+// Engineer: Dawei Sun
 // 
-// Create Date:    17:00:08 18/02/2014 
+// Create Date: 25.02.2022 00:16:27
 // Design Name: 
-// Module Name:    MouseReceiver 
+// Module Name: MouseReceiver 
 // Project Name: 
 // Target Devices: 
-// Tool versions: 
+// Tool Versions: 
 // Description: 
 //
 // Dependencies: 
 //
 // Revision: 
 // Revision 0.01 - File Created
-// Additional Comments: 	Large ammounts of code were already provided, however
-//									that also greatly restricts my coding options
+// Additional Comments:
 //
 //////////////////////////////////////////////////////////////////////////////////
-module MouseReceiver(
-	//Standard Inputs
-	input 				RESET,
-	input 				CLK,
-	//Mouse IO - CLK
-	input 				CLK_MOUSE_IN,
-	//Mouse IO - DATA
-	input 				DATA_MOUSE_IN,
-	//Control
-	input 				READ_ENABLE,
-	output [7:0] 		BYTE_READ,
-	output [1:0] 		BYTE_ERROR_CODE,
-	output 				BYTE_READY
+
+
+module MouseReceiver (
+    // Standard Inputs
+    input CLK,
+    input RESET,
+    // Mouse IO
+    input CLK_MOUSE_IN,
+    input DATA_MOUSE_IN,
+    // Control
+    input READ_ENABLE,
+    output [7:0] BYTE_READ,
+    output [1:0] BYTE_ERROR_CODE,
+    output BYTE_READY
 );
 
-	//////////////////////////////////////////////////////////
-	// Clk Mouse delayed to detect clock edges
-	reg ClkMouseInDly;
-	always@(posedge CLK)
-		ClkMouseInDly <= CLK_MOUSE_IN;
-	
-	//////////////////////////////////////////////////////////
-	//A simple state machine to handle the incoming 11-bit codewords
-	reg [2:0] 			Curr_State, Next_State;
-	reg [7:0] 			Curr_MSCodeShiftReg, Next_MSCodeShiftReg;
-	reg [3:0] 			Curr_BitCounter, Next_BitCounter;
-	reg 					Curr_ByteReceived, Next_ByteReceived;
-	reg [1:0] 			Curr_MSCodeStatus, Next_MSCodeStatus;
-	reg [15:0] 			Curr_TimeoutCounter, Next_TimeoutCounter;
-	
-	//Sequential
-	always@(posedge CLK) 
-		begin
-			if(RESET) 
-				begin
-					Curr_State 				<= 0;
-					Curr_MSCodeShiftReg 	<= 8'h00;
-					Curr_BitCounter 		<= 0;
-					Curr_ByteReceived 	<= 1'b0;
-					Curr_MSCodeStatus 	<= 2'b00;
-					Curr_TimeoutCounter 	<= 0;
-				end 
-			else 
-				begin
-					Curr_State 				<= Next_State;
-					Curr_MSCodeShiftReg 	<= Next_MSCodeShiftReg;
-					Curr_BitCounter 		<= Next_BitCounter;
-					Curr_ByteReceived 	<= Next_ByteReceived;
-					Curr_MSCodeStatus 	<= Next_MSCodeStatus;
-					Curr_TimeoutCounter 	<= Next_TimeoutCounter;
-				end
-		end
-	
-	//Combinatorial
-	always@* 
-		begin
-			//defaults to make the State Machine more readable
-			Next_State 				= Curr_State;
-			Next_MSCodeShiftReg 	= Curr_MSCodeShiftReg;
-			Next_BitCounter 		= Curr_BitCounter;
-			Next_ByteReceived 	= 1'b0;
-			Next_MSCodeStatus 	= Curr_MSCodeStatus;
-			Next_TimeoutCounter 	= Curr_TimeoutCounter + 1'b1;
-			
-			//The states
-			case (Curr_State)
-				0: 
-					begin
-						//Falling edge of Mouse clock and MouseData is low i.e. start bit
-						if(READ_ENABLE & ClkMouseInDly & ~CLK_MOUSE_IN & ~DATA_MOUSE_IN) 
-							begin
-								Next_State 						= 1;
-								Next_MSCodeStatus 			= 2'b00;
-							end
-							
-						Next_BitCounter 					= 0;
-					end
-				
-				// Read successive byte bits from the mouse here
-				1: 
-					begin
-						if(Curr_TimeoutCounter == 50000) // 1ms timeout
-							Next_State 						= 0;
-						else if(Curr_BitCounter == 8)
-							begin // if last bit go to parity bit check
-								Next_State 						= 2;
-								Next_BitCounter 				= 0;
-							end 
-						else if(ClkMouseInDly & ~CLK_MOUSE_IN)
-							begin //Shift Byte bits in
-								Next_MSCodeShiftReg[6:0] 	= Curr_MSCodeShiftReg[7:1];
-								Next_MSCodeShiftReg[7] 		= DATA_MOUSE_IN;
-								Next_BitCounter 				= Curr_BitCounter + 1;
-								Next_TimeoutCounter 			= 0;
-							end
-					end
-				
-				//Check Parity Bit
-				2: 
-					begin
-						//Falling edge of Mouse clock and MouseData is odd parity
-						if(Curr_TimeoutCounter == 50000)
-							Next_State 						= 0;
-						else if(ClkMouseInDly & ~CLK_MOUSE_IN)
-							begin
-								if (DATA_MOUSE_IN != ~^Curr_MSCodeShiftReg[7:0]) // Parity bit error returns error status on 1
-									Next_MSCodeStatus[0] 	= 1'b1;
-									
-								Next_BitCounter 			= 0;	// redundant, set this way by previous state
-								Next_State 					= 3;
-								Next_TimeoutCounter 		= 0;
-							end
-					end
-		/*
-		Fill in the code for State 3'b011 to detect the Stop bit and set MSCodeStatus [1] accordingly, the final
-		State 3'b100, as well as default State
-		*/
-		/*
-		¡­¡­¡­¡­¡­¡­..
-		FILL IN THIS AREA
-		¡­¡­¡­¡­¡­¡­.
-		*/
-				3: 
-					begin
-						//Falling edge of Mouse clock and MouseData is stop bit
-						if(Curr_TimeoutCounter == 100000)
-							Next_State 									= 0;
-						else if(ClkMouseInDly & ~CLK_MOUSE_IN) 
-							begin
-								if (DATA_MOUSE_IN) 
-									Next_MSCodeStatus[1] 			= 1'b0;	// stop bit should be high
-								else
-									Next_MSCodeStatus[1] 			= 1'b1;
-									
-								Next_State 								= 4;
-								Next_TimeoutCounter 					= 0;
-							end
-					end	
-				
-				4:
-					begin // not sure why this state can't be integrated into state 011
-						//Falling edge of Mouse clock and MouseData is high i.e. stop bit
-						Next_ByteReceived 					= 1'b1;		// set BYTE_READY flag for the master fsm to read the byte	
-						Next_State 								= 0;	// GOTO beginning of the FSM
-					end
-				
-				default:
-					begin
-						// effectively the same as if RESET was set i.e. restarts FSM from ground state if we somehow escape the real states
-						Next_State 								= 0;
-						Next_MSCodeShiftReg 					= 8'h00;
-						Next_BitCounter 						= 0;
-						Next_ByteReceived 					= 1'b0;
-						Next_MSCodeStatus 					= 2'b00;
-						Next_TimeoutCounter 					= 0;	
-					end
-		
-			endcase
-		end
 
-	assign BYTE_READY 		= Curr_ByteReceived;
-	assign BYTE_READ 			= Curr_MSCodeShiftReg;
-	assign BYTE_ERROR_CODE 	= Curr_MSCodeStatus;
-	
+    parameter T_TIMEOUT = 100000;
+
+
+    reg CLK_MOUSE_SYNC;
+    always @(posedge CLK) begin
+        CLK_MOUSE_SYNC <= CLK_MOUSE_IN;
+    end
+
+
+    reg [2:0] curr_state, next_state;
+    reg [7:0] curr_MSShiftReg, next_MSShiftReg;
+    reg [3:0] curr_bitCtr, next_bitCtr;
+    reg curr_byteReceived, next_byteReceived;
+    reg [1:0] curr_MSStatus, next_MSStatus;
+    reg [15:0] curr_timeoutCtr, next_timeoutCtr;
+
+    // Sequential
+    always @(posedge CLK or posedge RESET) begin
+            if (RESET) begin
+                curr_state <= 0;
+                curr_MSShiftReg <= 8'h00;
+                curr_bitCtr <= 0;
+                curr_byteReceived <= 1'b0;
+                curr_MSStatus <= 2'b00;
+                curr_timeoutCtr <= 0;
+            end
+            else begin
+                curr_state <= next_state;
+                curr_MSShiftReg <= next_MSShiftReg;
+                curr_bitCtr <= next_bitCtr;
+                curr_byteReceived <= next_byteReceived;
+                curr_MSStatus <= next_MSStatus;
+                curr_timeoutCtr <= next_timeoutCtr;
+            end
+        end
+
+    // Combinational
+    always @(*) begin
+        next_state = curr_state;
+        next_MSShiftReg = curr_MSShiftReg;
+        next_bitCtr = curr_bitCtr;
+        next_byteReceived = 1'b0;
+        next_MSStatus = curr_MSStatus;
+        next_timeoutCtr = curr_timeoutCtr + 1;
+        
+        case (curr_state)
+            0 : begin
+                if (READ_ENABLE & CLK_MOUSE_SYNC & ~CLK_MOUSE_IN & ~DATA_MOUSE_IN) begin
+                    next_state = 1;
+                    next_MSStatus = 2'b00;
+                end
+                next_bitCtr = 0;
+            end
+            1 : begin
+                if (curr_timeoutCtr == 50000) begin // 1ms timeout
+                    next_state = 0;
+                end
+                else if(curr_bitCtr == 8) begin
+                    next_state = 2;
+                    next_bitCtr = 0;
+                end
+                else if (CLK_MOUSE_SYNC & ~CLK_MOUSE_IN) begin
+                    next_MSShiftReg[6:0] = curr_MSShiftReg[7:1];
+                    next_MSShiftReg[7] = DATA_MOUSE_IN;
+                    next_bitCtr = curr_bitCtr + 1;
+                    next_timeoutCtr = 0;
+                end
+            end
+            2 : begin
+                if (curr_timeoutCtr == 50000) begin
+                    next_state = 0;
+                end
+                else if (CLK_MOUSE_SYNC & ~CLK_MOUSE_IN) begin
+                    if (DATA_MOUSE_IN != (~^curr_MSShiftReg[7:0])) begin // parity bit error
+                        next_MSStatus[0] = 1'b1;
+                    end
+                    next_state = 3;
+                    next_bitCtr = 0;
+                    next_timeoutCtr = 0;
+                end
+            end
+            3 : begin
+                if (curr_timeoutCtr == T_TIMEOUT) begin
+                    next_state = 0;
+                end
+                else if (CLK_MOUSE_SYNC & ~CLK_MOUSE_IN) begin
+                    if (DATA_MOUSE_IN) begin
+                        next_MSStatus[1] = 1'b0;
+                    end
+                    else begin // stop bit missing
+                        next_MSStatus[1] = 1'b1;
+                    end
+                    next_state = 4;
+                    next_timeoutCtr = 0;
+                end
+            end
+            4 : begin
+                next_byteReceived = 1'b1;
+                next_state = 0;
+            end
+            default : begin
+                next_state = 0;
+                next_MSShiftReg = 8'h00;
+                next_bitCtr = 0;
+                next_byteReceived = 1'b0;
+                next_MSStatus = 2'b00;
+                next_timeoutCtr = 0;
+            end
+        endcase
+    end
+
+
+    assign BYTE_READY = curr_byteReceived;
+    assign BYTE_READ = curr_MSShiftReg;
+    assign BYTE_ERROR_CODE = curr_MSStatus;
+
 endmodule
